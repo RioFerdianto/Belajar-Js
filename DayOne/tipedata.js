eac659a1c8859d73ad496229119c93ff24bdd3c4
Rio Ferdianto <emailgithubkamu@gmail.com>
2026-01-17 21:10:57 +0700

    Update belajar js

---
+++ DayOne/tipedata.js
@@ -0,0 +1,15 @@
+// Belajar tipe data pada JavaScript
+// String "isi dengan teks"
+// Number 12345
+// Boolean true atau false
+// Array [1, 2, 3, "empat", "lima"]
+// Object {nama: "Andi", umur: 25}
+
+let nama = "Rio"; // String
+console.log(typeof nama);
+
+let dataNumber = 2024; // Number
+console.log(typeof dataNumber);
+
+let dataBoolean = false; // Boolean
+console.log(typeof dataBoolean);
